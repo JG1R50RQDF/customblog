14b3d754bdcd33395d6c19841a96ef4bf7dc240b
Muazzam Mullah <zmz.muazzammullah@gmail.com>
2019-02-14 01:25:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var infobox=document.getElementById("info-box");
 
 infobox.addEventListener("load",function(){
 	var OurRequest= new XMLHttpRequest();
-	OurRequest.open("GET","https://api.population.io/1.0/population/World/today-and-tomorrow/?format=json");
+	OurRequest.open("GET","https://financialmodelingprep.com/api/company/price/AAPL");
 	OurRequest.onload=function(){
 		var ourData=JSON.parse(OurRequest.responseText);
 		//console.log(ourData);
