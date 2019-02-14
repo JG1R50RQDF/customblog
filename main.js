4cac62c897a99a0916214f4e391196222233d2d1
Muazzam Mullah <zmz.muazzammullah@gmail.com>
2019-02-14 02:05:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var btn=document.getElementById("loadButton");
 
 btn.addEventListener("click",function(){
 	var OurRequest= new XMLHttpRequest();
-	OurRequest.open("GET","https://financialmodelingprep.com/api/company/price/AAPL");
+	OurRequest.open("GET","https://api.exchangeratesapi.io/latest?base=USD");
 	OurRequest.onload=function(){
 		var ourData=JSON.parse(OurRequest.responseText);
 		console.log(ourData);
@@ -17,7 +17,7 @@ btn.addEventListener("click",function(){
 function render_data(data){
 	//console.log(data.total_population);
 	var data_string = "";
-	data_string+=("<h5>"+" Price- "+data.AAPL.price+"</h5>");
+	data_string+=("<h5>"+" 1usd- "+data.rates.zar+"</h5>");
 	infobox.insertAdjacentHTML('beforeend',data_string);
 
 }
