ca7f73211912780d54cbabfc5d5203b4207a4afd
Muazzam Mullah <zmz.muazzammullah@gmail.com>
2019-03-27 01:19:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function render_data(data_all){
 
 	var data_string = "<table style='width:100%;text-align: left;'><tr><th>Country</th><th>lrpmg</th><th>Year</th></tr>";
 	for(x=0;x<data_all.data.length;x++){
-		data_string+=("<tr><td>"+data_all.data.[x].country+"</td><td>"+data_all.data[x].lrpmg+"</td><td>"+data_all.data[x].year+"</td><tr>");
+		data_string+=("<tr><td>"+data_all.data[x].country+"</td><td>"+data_all.data[x].lrpmg+"</td><td>"+data_all.data[x].year+"</td><tr>");
 	}
 	
 	infobox.insertAdjacentHTML('beforeend',data_string+"</table>");
