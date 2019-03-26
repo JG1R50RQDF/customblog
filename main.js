156b8c1846c25bdd1b412152f9f4085ec4662fb4
Muazzam Mullah <zmz.muazzammullah@gmail.com>
2019-03-27 00:02:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var btn=document.getElementById("loadButton");
 
 window.addEventListener("load",function(){
 	var OurRequest= new XMLHttpRequest();
-	OurRequest.open("GET","https://accounts.google.com/ServiceLogin?passive=1209600&continue=https://script.google.com/macros/s/AKfycbyk7lEvp_IW0PBMLZ0iHE3Gkw05cYW65Bs-kcuyekkMOcjNFGM/exec/consumption?callback%3Dctrlq&followup=https://script.google.com/macros/s/AKfycbyk7lEvp_IW0PBMLZ0iHE3Gkw05cYW65Bs-kcuyekkMOcjNFGM/exec/consumption?callback%3Dctrlq");
+	OurRequest.open("GET","https://script.google.com/macros/s/AKfycbyP5Rifn7Q05Qcd7CTfm-AOouFHHvUAvCVVuKSfQu-LCqJocP8/exec?key=abcdef");
 	OurRequest.send();
 
 });
@@ -18,7 +18,3 @@ function render_data(data){
 	// infobox.insertAdjacentHTML('beforeend',data_string);
 
 }
-  function ctrlq(e) {
-    console.log(e.result)
-  }
-
