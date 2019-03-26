8dd97c4fee14f2494622874779c7c5005359217c
Muazzam Mullah <zmz.muazzammullah@gmail.com>
2019-03-27 01:13:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,10 @@ window.addEventListener("load",function(){
 function render_data(data){
 
 	var data_string = "<table style='width:100%;text-align: left;'><tr><th>Country</th><th>lrpmg</th><th>Year</th></tr>";
-	data_string+=("<tr><td>Austria</td><td>18.9999</td><td>1978</td><tr>");
+	for(x=0;x<data.length;x++){
+		data_string+=("<tr><td>"+data[x].country+"</td><td>"+data[x].lrpmg+"</td><td>"+data[x].year+"</td><tr>");
+	}
+	
 	infobox.insertAdjacentHTML('beforeend',data_string+"</table>");
 
 }
